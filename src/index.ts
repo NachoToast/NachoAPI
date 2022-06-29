1440bb855682b8e14d59641d704e3f148442df0d
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-06-29 16:42:56 +1200

    update config import

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { Config } from './@types/Config';
 import { Server } from './Server';
 
-import config from '../config.json';
+const config: Config = require('../config.json');
 const version = process.env.npm_package_version || require('../package.json').version;
 
-export const server = new Server(version, config as Config);
+export const server = new Server(version, config);

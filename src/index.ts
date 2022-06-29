e9b4e8a10cabb31033f19f76171f2faba44ab237
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-06-29 16:13:16 +1200

    syntax updates and comments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { Server } from './classes/Server';
 import { Config } from './types/Config';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const config: Config = require('../config.json');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const version = process.env.npm_package_version || require('../package.json').version;
 
 export const server = new Server({ config, version });
--- src/types/Config.ts
+++ src/types/Config.ts
@@ -2,11 +2,10 @@ export interface Config {
     /** Port the API will listen on. */
     port: number;
 
-    /**
-     * You can disable modules individually by changing the `disabled` key to true.
-     */
+    /** You can disable modules individually by changing the `disabled` key to true. */
     modules: {
-        /** Spotify module of the API, used for Client Credentials OAuth flow on
+        /**
+         * Spotify module of the API, used for Client Credentials OAuth flow on
          * any NachoToast applications that involve Spotify.
          */
         spotify: {

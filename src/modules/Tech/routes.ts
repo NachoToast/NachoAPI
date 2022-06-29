cc577caaf394aefd0e2fd80e53815ba69018c421
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-06-29 16:14:43 +1200

    idk

--- src/modules/Tech/routes.ts
+++ src/modules/Tech/routes.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import { join } from 'path';
-import { ParseClient } from './parseClient';
-import { SearchClient } from './searchClient';
 
 export function routes(router: Router) {
     router.get('/tech', (req, res) => res.status(200).json('Tech base endpoint'));
 
     // new SearchClient('https://www.pbtech.co.nz/category/peripherals/monitors/shop-all');
     // new SearchClient('https://www.pbtech.co.nz/category/peripherals/monitors/shop-all?pg=1');
-
-    new ParseClient(join(__dirname, 'monitors.json'));
+    // new ParseClient(join(__dirname, 'monitors.json'));
 }

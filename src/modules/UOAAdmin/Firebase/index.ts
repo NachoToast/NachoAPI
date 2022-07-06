5847d3de175a44a689a9e17c4a83fdf580744443
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-07-06 22:44:21 +1200

    remove uoa admin firebase files temporarily

--- src/modules/UOAAdmin/Firebase/index.ts
+++ src/modules/UOAAdmin/Firebase/index.ts
@@ -1,25 +1,25 @@
-import admin from 'firebase-admin';
-import { getDatabase } from 'firebase-admin/database';
+// import admin from 'firebase-admin';
+// import { getDatabase } from 'firebase-admin/database';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const serviceAccount = require(`./serviceAccountKey.json`);
+// const serviceAccount = require(`./serviceAccountKey.json`);
 
-export const app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://nachoapi-ldab-default-rtdb.asia-southeast1.firebasedatabase.app`,
-});
+// export const app = admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: `https://nachoapi-ldab-default-rtdb.asia-southeast1.firebasedatabase.app`,
+// });
 
-export const db = getDatabase(app);
-const ref = db.ref(`server/saving-data/fireblog`);
+// export const db = getDatabase(app);
+// const ref = db.ref(`server/saving-data/fireblog`);
 
-const usersRef = ref.child(`users`);
-usersRef.set({
-    alanisawesome: {
-        date_of_birth: `June 23, 1912`,
-        full_name: `Alan Turing`,
-    },
-    gracehop: {
-        date_of_birth: `December 9, 1906`,
-        full_name: `Grace Hopper`,
-    },
-});
+// const usersRef = ref.child(`users`);
+// usersRef.set({
+//     alanisawesome: {
+//         date_of_birth: `June 23, 1912`,
+//         full_name: `Alan Turing`,
+//     },
+//     gracehop: {
+//         date_of_birth: `December 9, 1906`,
+//         full_name: `Grace Hopper`,
+//     },
+// });

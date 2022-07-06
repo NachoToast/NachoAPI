d6551bab5252eda61ea4b738efd67ed51d3de92d
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-07-06 23:24:20 +1200

    Add help page for quotes

--- src/modules/QuoteAPI/routes.ts
+++ src/modules/QuoteAPI/routes.ts
@@ -15,6 +15,20 @@ function getQuoteById(id: string): Quote | string {
 
 export function routes(router: Router) {
     router.get(`/quotes`, (req, res) => res.status(200).json(getRandomQuote()));
+    router.get(`/quotes/help`, (req, res) => {
+        const output: string[] = [
+            `Welcome to the quotes API, endpoints are:`,
+            `GET /quotes -> Returns a random quote`,
+            `GET /quotes/help -> Returns this`,
+            `GET /quotes/all -> Returns all quotes in dictionary form`,
+            `GET /quotes/:id -> Returns a specific quote, e.g. '/quotes/0'`,
+        ];
+        res.status(200).send(
+            `<div style='white-space: pre-wrap'>Welcome to the quotes API, available endpoints are: ${output.join(
+                `\n`,
+            )}</div>`,
+        );
+    });
     router.get(`/quotes/all`, (req, res) => res.status(200).json(quotes));
     router.get(`/quotes/:id`, (req, res) => res.status(200).json(getQuoteById(req.params.id)));
 }
